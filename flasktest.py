e12faa8459a36f73543830efe4b098f012e09eac
Ian <ian@ian.Belkin>
2014-01-15 00:46:45 -0500

    Initial

--- flasktest.py
+++ flasktest.py
@@ -4,7 +4,7 @@ from forms import *
 
 app = Flask(__name__)
 app.secret_key = 'secret_shhhhh'
-
+app.debug = True  # TODO: IMPORTANT >> Remove this before pushing live!!!!!!
 
 test_item_dict = {}
 for i in range(100, 201):
@@ -16,7 +16,7 @@ for i in range(1, 21):
 
 test_bin_dict = {}
 for i in range(1, 27):
-    test_bin_dict[i] = ['CD' + str(i)]
+    test_bin_dict[i] = 'CD' + str(i)
 
 
 @app.route('/')
@@ -48,9 +48,10 @@ def shelves():
         if form.validate():
             test_shelf_dict[form.name.data] = form.name.data
             form.name.data = ''
-            return render_template('shelves.html', form=form, test_shelf_dict_=test_shelf_dict, shelf_added=True)
+            return render_template('shelves.html', form=form, test_shelf_dict=test_shelf_dict, shelf_added=True)
         else:
             return render_template('shelves.html', form=form, test_shelf_dict=test_shelf_dict, shelf_added=False)
+
 
 @app.route('/bins', methods=['GET', 'POST'])
 def bins():
@@ -61,7 +62,8 @@ def bins():
         if form.validate():
             test_bin_dict[len(test_bin_dict) + 1] = form.name.data
             form.name.data = ''
-            return render_template('bins.html', form=form, test_bin_dict_=test_shelf_dict, shelf_added=True)
+            form.shelf_name = ''
+            return render_template('bins.html', form=form, test_bin_dict=test_shelf_dict, shelf_added=True)
         else:
             return render_template('bins.html', form=form, test_bin_dict=test_shelf_dict, shelf_added=False)
 
